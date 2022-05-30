ab7f3b416d5127bf4b5cf79d90866391060416e2
chiaki22 <eriguchiaki227@gmail.com>
2022-05-30 22:57:01 +0900

    新規投稿保存機能の実装

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -4,6 +4,21 @@ class WorksController < ApplicationController
   end
 
   def new
+    @work = Work.new
   end
-  
+
+  def create
+    @work = Work.new(work_params)
+    if @work.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def work_params
+    params.require(:work).permit(:title, :category_id, :day, :star_id, :memo, :image)
+  end
 end
--- app/models/work.rb
+++ app/models/work.rb
@@ -2,7 +2,11 @@ class Work < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category, :star
 
-  validates :title, :day,
-  validates :category_id, :star_id numericality: { other_than: 1 , message: "を選択してください"} 
+  has_one_attached :image
+
+  validates :title, :day, presence: true
+  validates :title, length: { maximum: 40 }
+  validates :memo, length: { maximum: 1000 }
+  validates :category_id, :star_id, numericality: { other_than: 1, message: 'を選択してください'} 
 
 end
